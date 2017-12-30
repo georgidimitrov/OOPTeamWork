a9a5bd82e42e92fc1555682f575e205d9db96f3a
Dimitar Petrov <dimitar.petrov.311@gmail.com>
2017-12-30 15:37:24 +0200

    Interface ICollisionInfo

---
+++ AlphaTank/AlphaTank.Program/Display/Contracts/ICollisionInfo.cs
@@ -0,0 +1,11 @@
+﻿using AlphaTank.Program.Models.GameObjects.Common;
+
+namespace AlphaTank.Program.Display.Contracts
+{
+    public interface ICollisionInfo
+    {
+        bool IsCollided { get; }
+
+        GameObjectType Type { get; }
+    }
+}
