dac223d0bb38d0bd8bb639017d441b4ce089fd25
Dimitar Petrov <dimitar.petrov.311@gmail.com>
2017-12-30 15:33:32 +0200

    CollisionInfo: vmesto true/false za collisiona shell i tank vrushtat tova info

---
+++ AlphaTank/AlphaTank.Program/Display/CollisionInfo.cs
@@ -0,0 +1,20 @@
+﻿using AlphaTank.Program.Display.Contracts;
+using AlphaTank.Program.Models.GameObjects.Common;
+
+namespace AlphaTank.Program.Display
+{
+    public class CollisionInfo : ICollisionInfo
+    {
+        public CollisionInfo(bool isCollided, GameObjectType type = GameObjectType.Road)
+        {
+            this.IsCollided = isCollided;
+            if (isCollided)
+            {
+                this.Type = type;
+            }
+        }
+
+        public bool IsCollided { get; }
+        public GameObjectType Type { get; }
+    }
+}
